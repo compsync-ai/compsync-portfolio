25c9c4bb3ff59c2febdc60a18c76b34d28c8b346
compsync-ai <compsync.ai@google.com>
2026-03-25 23:59:13 -0400

    chore: removed landing page reference for rulebook release

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,7 @@
 <script setup>
-import { computed } from "vue";
 import DocumentationPage from "./pages/DocumentationPage.vue";
-import LandingPage from "./pages/LandingPage.vue";
-import NotFoundPage from "./pages/NotFoundPage.vue";
-
-const appBasePath = (import.meta.env.BASE_URL || "/").replace(/\/+$/, "");
-const currentPath = computed(() => {
-  const pathname = window.location.pathname.replace(/\/+$/, "") || "/";
-
-  if (!appBasePath || appBasePath === "/") {
-    return pathname;
-  }
-
-  if (pathname === appBasePath) {
-    return "/";
-  }
-
-  if (pathname.startsWith(`${appBasePath}/`)) {
-    return pathname.slice(appBasePath.length) || "/";
-  }
-
-  return pathname;
-});
-
-const routeName = computed(() => {
-  if (currentPath.value === "/") return "home";
-  if (["/rulebook", "/rule-book"].includes(currentPath.value)) return "rulebook";
-  return "not-found";
-});
 </script>
 
 <template>
-  <LandingPage v-if="routeName === 'home'" />
-  <DocumentationPage v-else-if="routeName === 'rulebook'" />
-  <NotFoundPage v-else />
+  <DocumentationPage />
 </template>
